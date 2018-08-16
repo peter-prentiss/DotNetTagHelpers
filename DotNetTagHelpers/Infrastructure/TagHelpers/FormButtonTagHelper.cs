0ca1e5dd528a3a575f5e2104e86aa9ea1615de5e
Peter <peter.prentiss@gmail.com>
2018-08-16 12:05:49 -0500

    Create FormButtonTagHelper class

---
+++ DotNetTagHelpers/Infrastructure/TagHelpers/FormButtonTagHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace DotNetTagHelpers.Infrastructure.TagHelpers
+{
+    public class FormButtonTagHelper
+    {
+        public FormButtonTagHelper()
+        {
+        }
+    }
+}
