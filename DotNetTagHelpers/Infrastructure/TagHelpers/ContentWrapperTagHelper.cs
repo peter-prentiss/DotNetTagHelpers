e2ecc2666382bf74b0d4f7d95f2e4501e9392350
Peter <peter.prentiss@gmail.com>
2018-08-19 12:11:22 -0500

    Update ContentWrapperTagHelper

--- DotNetTagHelpers/Infrastructure/TagHelpers/ContentWrapperTagHelper.cs
+++ DotNetTagHelpers/Infrastructure/TagHelpers/ContentWrapperTagHelper.cs
@@ -1,10 +1,33 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
 namespace DotNetTagHelpers.Infrastructure.TagHelpers
 {
-    public class ContentWrapperTagHelper
+    [HtmlTargetElement("div", Attributes = "title")]
+    public class ContentWrapperTagHelper : TagHelper
     {
-        public ContentWrapperTagHelper()
+        public bool IncludeHeader { get; set; } = true;
+        public bool IncludeFooter { get; set; } = true;
+
+        public string Title { get; set; }
+
+        public override void Process(TagHelperContext context,
+                                    TagHelperOutput output)
         {
+            output.Attributes.SetAttribute("class", "panel-body");
+            TagBuilder title = new TagBuilder("h1");
+            title.InnerHtml.Append(Title);
+            TagBuilder container = new TagBuilder("div");
+            container.Attributes["class"] = "bg-info panel-body";
+            container.InnerHtml.AppendHtml(title);
+            if (IncludeHeader)
+            {
+                output.PreElement.SetHtmlContent(container);
+            }
+            if (IncludeFooter)
+            {
+                output.PostElement.SetHtmlContent(container);
+            }
         }
     }
 }
