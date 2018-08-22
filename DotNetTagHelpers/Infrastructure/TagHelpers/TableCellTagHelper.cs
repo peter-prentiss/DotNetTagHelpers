7a23acabd85141f1549275b155183795223e0c4d
Peter <peter.prentiss@gmail.com>
2018-08-22 12:15:46 -0500

    Update TableCellTagHelper

--- DotNetTagHelpers/Infrastructure/TagHelpers/TableCellTagHelper.cs
+++ DotNetTagHelpers/Infrastructure/TagHelpers/TableCellTagHelper.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
 namespace DotNetTagHelpers.Infrastructure.TagHelpers
 {
-    public class TableCellTagHelper
-    {
-        public TableCellTagHelper()
+    [HtmlTargetElement("td", Attributes = "wrap")]
+    public class TableCellTagHelper : TagHelper {
+        public override void Process(TagHelperContext context,
+                                    TagHelperOutput output)
         {
+            output.PreContent.SetHtmlContent("<b><i>");
+            output.PostContent.SetHtmlContent("</i></b>");
         }
     }
 }
