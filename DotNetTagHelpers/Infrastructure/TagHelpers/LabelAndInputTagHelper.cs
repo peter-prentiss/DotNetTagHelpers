8a66936b0f9f5423724097bffdea86ec15a806ff
Peter <peter.prentiss@gmail.com>
2018-08-28 13:17:04 -0500

    Update LabelAndInputTagHelper

--- DotNetTagHelpers/Infrastructure/TagHelpers/LabelAndInputTagHelper.cs
+++ DotNetTagHelpers/Infrastructure/TagHelpers/LabelAndInputTagHelper.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
 namespace DotNetTagHelpers.Infrastructure.TagHelpers
 {
-    public class LabelAndInputTagHelper
+    [HtmlTargetElement("label", Attributes = "helper-for")]
+    [HtmlTargetElement("input", Attributes = "helper-for")]
+    public class LabelAndInputTagHelper : TagHelper
     {
-        public LabelAndInputTagHelper()
+        public ModelExpression HelperFor { get; set; }
+        public override void Process(TagHelperContext context,
+        TagHelperOutput output)
         {
+            if (output.TagName == "label")
+            {
+                output.TagMode = TagMode.StartTagAndEndTag;
+                output.Content.Append(HelperFor.Name);
+                output.Attributes.SetAttribute("for", HelperFor.Name);
+            }
+            else if (output.TagName == "input")
+            {
+                output.TagMode = TagMode.SelfClosing;
+                output.Attributes.SetAttribute("name", HelperFor.Name);
+                output.Attributes.SetAttribute("class", "form-control");
+                if (HelperFor.Metadata.ModelType == typeof(int?))
+                {
+                    output.Attributes.SetAttribute("type", "number");
+                }
+            }
         }
     }
 }
