c1aca7a31d9cd13ccd0c64b8373b1c3f43895199
Peter <peter.prentiss@gmail.com>
2018-08-31 13:22:39 -0500

    Update CoordinatingTagHelpers

--- DotNetTagHelpers/Infrastructure/TagHelpers/CoordinatingTagHelpers.cs
+++ DotNetTagHelpers/Infrastructure/TagHelpers/CoordinatingTagHelpers.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
 namespace DotNetTagHelpers.Infrastructure.TagHelpers
 {
-    public class CoordinatingTagHelpers
+    [HtmlTargetElement("div", Attributes = "theme")]
+    public class ButtonGroupThemeTagHelper : TagHelper
     {
-        public CoordinatingTagHelpers()
+        public string Theme { get; set; }
+        public override void Process(TagHelperContext context,
+        TagHelperOutput output)
         {
+            context.Items["theme"] = Theme;
+        }
+    }
+
+    [HtmlTargetElement("button", ParentTag = "div")]
+    [HtmlTargetElement("a", ParentTag = "div")]
+    public class ButtonThemeTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context,
+        TagHelperOutput output)
+        {
+            if (context.Items.ContainsKey("theme"))
+            {
+                output.Attributes.SetAttribute("class",
+                $"btn btn-{context.Items["theme"]}");
+            }
         }
     }
 }
