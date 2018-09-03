db4ce2f91378152969c8838cf37661f42f053785
Peter <peter.prentiss@gmail.com>
2018-09-03 13:32:07 -0500

    Add SelectiveTagHelper class

---
+++ DotNetTagHelpers/Infrastructure/TagHelpers/SelectiveTagHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace DotNetTagHelpers.Infrastructure.TagHelpers
+{
+    public class SelectiveTagHelper
+    {
+        public SelectiveTagHelper()
+        {
+        }
+    }
+}
