dbdae43dda03cd92983b081ea04a24532de443b1
Peter <peter.prentiss@gmail.com>
2018-09-10 13:38:38 -0500

    Add token validation to HomeController

--- DotNetTagHelpers/Controllers/HomeController.cs
+++ DotNetTagHelpers/Controllers/HomeController.cs
@@ -6,13 +6,18 @@ namespace DotNetTagHelpers.Controllers
     public class HomeController : Controller
     {
         private IRepository repository;
+
         public HomeController(IRepository repo)
         {
             repository = repo;
         }
+
         public ViewResult Index() => View(repository.Cities);
+
         public ViewResult Create() => View();
+
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Create(City city)
         {
             repository.AddCity(city);
