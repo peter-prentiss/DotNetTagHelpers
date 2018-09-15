d059c3154c460bc9148f84cad805221e21d21b4e
Peter <peter.prentiss@gmail.com>
2018-09-15 14:12:51 -0500

    apply formatting to city class

--- DotNetTagHelpers/Models/City.cs
+++ DotNetTagHelpers/Models/City.cs
@@ -1,9 +1,13 @@
-﻿namespace DotNetTagHelpers.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNetTagHelpers.Models
 {
     public class City
     {
         public string Name { get; set; }
         public string Country { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}", ApplyFormatInEditMode = true)]
         public int? Population { get; set; }
     }
 }
