72efe4a4d91ce9289f9a8455a83e5504516429fc
Peter <peter.prentiss@gmail.com>
2018-09-18 14:16:38 -0500

    Add display attribute to city class

--- DotNetTagHelpers/Models/City.cs
+++ DotNetTagHelpers/Models/City.cs
@@ -4,6 +4,7 @@ namespace DotNetTagHelpers.Models
 {
     public class City
     {
+        [Display(Name = "City")]
         public string Name { get; set; }
         public string Country { get; set; }
 
