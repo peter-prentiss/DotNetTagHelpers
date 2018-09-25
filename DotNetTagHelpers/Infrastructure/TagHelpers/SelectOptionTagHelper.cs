10895398688ffeecc9a6ee548be066bf5ed52101
Peter <peter.prentiss@gmail.com>
2018-09-25 14:25:12 -0500

    Update SelectOptionTagHelper

--- DotNetTagHelpers/Infrastructure/TagHelpers/SelectOptionTagHelper.cs
+++ DotNetTagHelpers/Infrastructure/TagHelpers/SelectOptionTagHelper.cs
@@ -1,10 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using DotNetTagHelpers.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
 namespace DotNetTagHelpers.Infrastructure.TagHelpers
 {
-    public class SelectOptionTagHelper
+    [HtmlTargetElement("select", Attributes = "model-for")]
+    public class SelectOptionTagHelper : TagHelper
     {
-        public SelectOptionTagHelper()
+        private IRepository repository;
+        public SelectOptionTagHelper(IRepository repo)
         {
+            repository = repo;
+        }
+        public ModelExpression ModelFor { get; set; }
+        public override async Task ProcessAsync(TagHelperContext context,
+        TagHelperOutput output)
+        {
+            output.Content.Append(
+            (await output.GetChildContentAsync(false)).GetContent());
+            object selected;
+            context.Items.TryGetValue(typeof(SelectTagHelper), out selected);
+            IEnumerable<string> selectedValues = (selected as IEnumerable<string>)
+            ?? Enumerable.Empty<string>();
+            PropertyInfo property = typeof(City)
+            .GetTypeInfo().GetDeclaredProperty(ModelFor.Name);
+            foreach (string country in repository.Cities
+            .Select(c => property.GetValue(c)).Distinct())
+            {
+                if (selectedValues.Any(s => s.Equals(country,
+                StringComparison.OrdinalIgnoreCase)))
+                {
+                    output.Content
+                    .AppendHtml($"<option selected>{country}</option>");
+                }
+                else
+                {
+                    output.Content.AppendHtml($"<option>{country}</option>");
+                }
+            }
         }
     }
 }
